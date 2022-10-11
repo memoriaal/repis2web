cb95ba5ec319a0d393b8c648199f5fd5da1e98c2
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2022-10-11 15:09:41 +0300

    Update repis_backup.sh

--- repis_backup.sh
+++ repis_backup.sh
@@ -6,8 +6,8 @@ echo $(date -u --iso-8601=seconds) Started repis backup
 
 mysql -u"${M_MYSQL_U}" -p"${M_MYSQL_P}" repis<<EOFMYSQL
 CALL backups.backup_table('repis.kirjed');
-CALL backups.backup_table('repis.kirjelipikud');
-CALL backups.backup_table('repis.kirjesildid');
+CALL backups.backup_table('repis.v_kirjelipikud');
+CALL backups.backup_table('repis.v_kirjesildid');
 EOFMYSQL
 
 echo $(date -u --iso-8601=seconds) Repis backup finished
