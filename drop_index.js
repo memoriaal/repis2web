f0a4847add4440b8eb1763eaddf89ee1066b74d9
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2022-11-17 18:07:57 +0200

    Create drop_index.js

---
+++ drop_index.js
@@ -0,0 +1,18 @@
+const ES_CREDENTIALS = process.env.ES_CREDENTIALS
+const INDEX = process.env.INDEX
+
+const elasticsearch = require('elasticsearch')
+const esOptions = { host: 'https://' + ES_CREDENTIALS + '@94abc9318c712977e8c684628aa5ea0f.us-east-1.aws.found.io:9243',
+                    requestTimeout: 1 * 60e3,
+                    log: 'trace' 
+                  }
+
+const esClient = new elasticsearch.Client(esOptions)
+
+esClient.indices.delete(
+  {index: INDEX},
+  function(err, resp, status) {
+    console.log("deleted", resp)
+    callback(null, resp)
+  }
+)
