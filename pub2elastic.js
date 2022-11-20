1b869d60ae80f2611975fdd7336444a556a3acb4
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2022-11-20 16:31:21 +0200

    repis7

--- pub2elastic.js
+++ pub2elastic.js
@@ -15,8 +15,8 @@ var csv = require("fast-csv");
 // data is expected as ["id", "kirje", "kirjed", "pereseosed", "tahvlikirje"]
 
 const elasticsearch = require('elasticsearch')
-// const esOptions = { host: 'https://' + ES_CREDENTIALS + '@94abc9318c712977e8c684628aa5ea0f.us-east-1.aws.found.io:9243',
-const esOptions = { host: 'https://' + ES_CREDENTIALS + '@repis8.es.eu-central-1.aws.cloud.es.io:9243',
+const esOptions = { host: 'https://' + ES_CREDENTIALS + '@repis7.us-east-1.aws.found.io:9243',
+// const esOptions = { host: 'https://' + ES_CREDENTIALS + '@repis8.es.eu-central-1.aws.cloud.es.io:9243',
                     requestTimeout: 1 * 60e3,
                     log: 'trace' 
                   }
