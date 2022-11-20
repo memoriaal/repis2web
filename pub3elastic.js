b8d4d3a43fc9898b95cd653fa1eb0939cad799cd
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2022-11-20 20:21:46 +0200

    Update pub3elastic.js

--- pub3elastic.js
+++ pub3elastic.js
@@ -70,60 +70,68 @@ async function run () {
     bulk.push(isik)
     if (cnt['all']%BULK_SIZE === 0) {
       console.log('read', cnt)
-      const operations = bulk.flatMap(doc => [{ index: { _index: INDEX, '_id': doc.id } }, doc])
-      const bulkResponse = await client.bulk({ refresh: true, operations })
 
-      if (bulkResponse.errors) {
-        const erroredDocuments = []
-        // The items array has the same order of the dataset we just indexed.
-        // The presence of the `error` key indicates that the operation
-        // that we did for the document has failed.
-        bulkResponse.items.forEach((action, i) => {
-          const operation = Object.keys(action)[0]
-          if (action[operation].error) {
-            erroredDocuments.push({
-              // If the status is 429 it means that you can retry the document,
-              // otherwise it's very likely a mapping error, and you should
-              // fix the document before to try it again.
-              status: action[operation].status,
-              error: action[operation].error,
-              foo: Object.keys(action),
-              // operation: body[i * 2],
-              // document: body[i * 2 + 1]
-            })
-          }
-        })
-        console.log(erroredDocuments)
-      }    
-      bulk = []
-    }
-
-    function row2isik(row) {
-      let isik = {}
-      isik['id'] = row[0]
-      isik['kirje'] = row[1]
-      isik['evokirje'] = row[2]
-      isik['perenimi'] = row[3]
-      isik['eesnimi'] = row[4]
-      isik['isanimi'] = row[5]
-      isik['emanimi'] = row[6]
-      if (row[7]) isik['sünd'] = row[7]
-      if (row[8]) isik['surm'] = row[8]
-      isik['kirjed'] = JSON.parse(row[9])
-      isik['pereseosed'] = JSON.parse(row[10])
-      isik['tahvlikirje'] = JSON.parse(row[11])
-      isik['isperson'] = row[12] ? 1 : 0
-      isik['kivi'] = row[13] ? 1 : 0
-      isik['emem'] = row[14] ? 1 : 0
-      isik['evo'] = row[15] ? 1 : 0
-      isik['wwii'] = row[16] ? 1 : 0
-      return isik
+      bulk = await bulk_upload(bulk)
     }
   })
-  .on('end', rowCount => {
+  .on('end', async rowCount => {
+    if(bulk.length > 0) {
+      await bulk_upload(bulk)
+    }
     console.log(`Parsed ${rowCount} rows`)
     console.log(cnt)
   })
   
 }
 run().catch(console.log)
+
+async function bulk_upload(bulk) {
+  const operations = bulk.flatMap(doc => [{ index: { _index: INDEX, '_id': doc.id } }, doc])
+  const bulkResponse = await client.bulk({ refresh: true, operations })
+  if (bulkResponse.errors) {
+    const erroredDocuments = []
+    // The items array has the same order of the dataset we just indexed.
+    // The presence of the `error` key indicates that the operation
+    // that we did for the document has failed.
+    bulkResponse.items.forEach((action, i) => {
+      const operation = Object.keys(action)[0]
+      if (action[operation].error) {
+        erroredDocuments.push({
+          // If the status is 429 it means that you can retry the document,
+          // otherwise it's very likely a mapping error, and you should
+          // fix the document before to try it again.
+          status: action[operation].status,
+          error: action[operation].error,
+          foo: Object.keys(action),
+          // operation: body[i * 2],
+          // document: body[i * 2 + 1]
+        })
+      }
+    })
+    console.log(erroredDocuments)
+  }
+  bulk = []
+  return bulk
+}
+
+function row2isik(row) {
+  let isik = {}
+  isik['id'] = row[0]
+  isik['kirje'] = row[1]
+  isik['evokirje'] = row[2]
+  isik['perenimi'] = row[3]
+  isik['eesnimi'] = row[4]
+  isik['isanimi'] = row[5]
+  isik['emanimi'] = row[6]
+  if (row[7]) isik['sünd'] = row[7]
+  if (row[8]) isik['surm'] = row[8]
+  isik['kirjed'] = JSON.parse(row[9])
+  isik['pereseosed'] = JSON.parse(row[10])
+  isik['tahvlikirje'] = JSON.parse(row[11])
+  isik['isperson'] = row[12] ? 1 : 0
+  isik['kivi'] = row[13] ? 1 : 0
+  isik['emem'] = row[14] ? 1 : 0
+  isik['evo'] = row[15] ? 1 : 0
+  isik['wwii'] = row[16] ? 1 : 0
+  return isik
+}
