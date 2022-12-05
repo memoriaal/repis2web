7bf4410411d79ab03a50316116b97a84ab8a60eb
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2022-12-05 13:29:33 +0200

    Move publishing logic to repis.kirjed

--- recreate_pub_nimekirjad.sql
+++ recreate_pub_nimekirjad.sql
@@ -27,82 +27,21 @@ COLLATE='utf8_estonian_ci'
 ENGINE=InnoDB
 ;
 
-INSERT ignore INTO pub.nimekirjad (persoon, kirje, perenimi, eesnimi, isanimi, emanimi, sünd, surm)
-SELECT persoon, kirje, perenimi, eesnimi, isanimi, emanimi, sünd, surm FROM repis.kirjed
+INSERT ignore INTO pub.nimekirjad (persoon, kirje, perenimi, eesnimi, isanimi, emanimi, sünd, surm
+     , kirjed
+     , isPerson, emem, kivi, wwiiref, evo)
+SELECT persoon, kirje, perenimi, eesnimi, isanimi, emanimi, sünd, surm
+     , repis.json_persoonikirjed(persoon)
+     , pub_isPerson, pub_emem, pub_kivi, pub_wwiiref, pub_evo
+  FROM repis.kirjed
  WHERE persoon = kirjekood
    AND persoon > 0
 ;
 
-UPDATE pub.nimekirjad
-SET kirjed = repis.json_persoonikirjed(persoon)
-;
 CALL pub.proc_pereseosed_nimekirja();
-
---
--- Not a person
-UPDATE pub.nimekirjad SET isPerson = 0;
-UPDATE pub.nimekirjad SET isPerson = 1 WHERE persoon IN
-(
-  SELECT DISTINCT persoon
-  FROM repis.kirjed k
-  WHERE k.allikas not in ('persoon')
-    and k.EkslikKanne = ''
-    and k.Peatatud = ''
-    and k.Puudulik = ''
-);
 
 --
 -- Värskenda kivitahvlite persoonid
 UPDATE import.memoriaal_kivitahvlid mt
 LEFT JOIN repis.kirjed k0 ON k0.kirjekood = mt.kirjekood
 SET mt.persoon = k0.persoon;
-
---
--- Kuulub memoriaali seinale
-UPDATE pub.nimekirjad 
-   SET kivi = 1
-     , tahvlikirje = ifnull(repis.json_tahvlikirje(persoon), JSON_OBJECT())
- WHERE persoon IN (
-  SELECT DISTINCT kirjekood AS persoon
-  FROM repis.v_kirjesildid
-  WHERE silt = 'x - kivi'
-  AND deleted_at = 0
-);
-
---
--- Kuulub e-memoriaalis avaldamisele
-UPDATE pub.nimekirjad SET emem = 1 WHERE persoon IN
-(
-  SELECT DISTINCT persoon
-  FROM repis.kirjed k
-  WHERE k.allikas IN (
-    SELECT kood FROM repis.allikad 
-    WHERE ifnull(nonPerson,0) = 0
-      AND kood NOT IN ('kirm','elk','PR','RR')
-  )
-  and k.EkslikKanne = ''
-  and k.Peatatud = ''
-  and k.Puudulik = ''
-);
--- mitterelevantsed muidugi mitte
-UPDATE pub.nimekirjad SET emem = 0 WHERE emem = 1 and persoon IN
-(
-  SELECT DISTINCT kirjekood AS persoon
-  FROM repis.v_kirjesildid
-  WHERE silt = 'x - mitterelevantne'
-  AND deleted_at = 0
-);
-
---
--- Avaldatud Eesti Vabariigi ohvitseride seinal
-UPDATE pub.nimekirjad nk
-RIGHT JOIN import.memoriaal_evo evo ON evo.persoon = nk.persoon
-SET nk.evo = 1, nk.evokirje = evo.kirje;
-
---
--- Kuulub II Maailmasõja põgenike nimekirja
-UPDATE pub.nimekirjad SET wwiiref = 1 WHERE persoon IN
-(
-  SELECT DISTINCT persoon FROM repis.kirjed
-  WHERE allikas IN ('kirm','elk')
-);
