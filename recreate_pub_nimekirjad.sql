d66eea31f692b5c11c4cbe61d321ef30132bb34d
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2022-12-05 10:43:53 +0200

    Update recreate_pub_nimekirjad.sql

--- recreate_pub_nimekirjad.sql
+++ recreate_pub_nimekirjad.sql
@@ -1,26 +1,26 @@
 CREATE OR REPLACE TABLE pub.nimekirjad (
-	persoon CHAR(10) NOT NULL COLLATE 'utf8_estonian_ci',
+  persoon CHAR(10) NOT NULL COLLATE 'utf8_estonian_ci',
   kirje TEXT NULL DEFAULT NULL COLLATE 'utf8_estonian_ci',
   evokirje TEXT NULL DEFAULT NULL COLLATE 'utf8_estonian_ci',
-	perenimi VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'utf8_estonian_ci',
-	eesnimi VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'utf8_estonian_ci',
-	isanimi VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'utf8_estonian_ci',
-	emanimi VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'utf8_estonian_ci',
-	sünd VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'utf8_estonian_ci',
-	surm VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'utf8_estonian_ci',
-	isPerson BIT(1) NOT NULL DEFAULT b'0',
-	kivi BIT(1) NOT NULL DEFAULT b'0',
-	emem BIT(1) NOT NULL DEFAULT b'0',
-	evo BIT(1) NOT NULL DEFAULT b'0',
-	wwiiref BIT(1) NOT NULL DEFAULT b'0',
-	kirjed LONGTEXT NOT NULL DEFAULT '[]' COLLATE 'utf8_estonian_ci',
-	pereseosed LONGTEXT NOT NULL DEFAULT '[]' COLLATE 'utf8_estonian_ci',
-	tahvlikirje LONGTEXT NOT NULL DEFAULT '[]' COLLATE 'utf8_estonian_ci',
-	PRIMARY KEY (persoon) USING BTREE,
-	CONSTRAINT FK_nimekirjad_repis_kirjed FOREIGN KEY (persoon) REFERENCES repis.kirjed (kirjekood) ON UPDATE CASCADE ON DELETE CASCADE,
-	CONSTRAINT `CC1` CHECK (json_valid(`kirjed`)),
-	CONSTRAINT `CC2` CHECK (json_valid(`pereseosed`)),
-	CONSTRAINT `CC3` CHECK (json_valid(`tahvlikirje`))
+  perenimi VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'utf8_estonian_ci',
+  eesnimi VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'utf8_estonian_ci',
+  isanimi VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'utf8_estonian_ci',
+  emanimi VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'utf8_estonian_ci',
+  sünd VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'utf8_estonian_ci',
+  surm VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'utf8_estonian_ci',
+  isPerson BIT(1) NOT NULL DEFAULT b'0',
+  kivi BIT(1) NOT NULL DEFAULT b'0',
+  emem BIT(1) NOT NULL DEFAULT b'0',
+  evo BIT(1) NOT NULL DEFAULT b'0',
+  wwiiref BIT(1) NOT NULL DEFAULT b'0',
+  kirjed LONGTEXT NOT NULL DEFAULT '[]' COLLATE 'utf8_estonian_ci',
+  pereseosed LONGTEXT NOT NULL DEFAULT '[]' COLLATE 'utf8_estonian_ci',
+  tahvlikirje LONGTEXT NOT NULL DEFAULT '[]' COLLATE 'utf8_estonian_ci',
+  PRIMARY KEY (persoon) USING BTREE,
+  CONSTRAINT FK_nimekirjad_repis_kirjed FOREIGN KEY (persoon) REFERENCES repis.kirjed (kirjekood) ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT `CC1` CHECK (json_valid(`kirjed`)),
+  CONSTRAINT `CC2` CHECK (json_valid(`pereseosed`)),
+  CONSTRAINT `CC3` CHECK (json_valid(`tahvlikirje`))
 )
 COMMENT='Persoonide kuulumine erinevatesse avaldatud nimekirjadesse'
 COLLATE='utf8_estonian_ci'
@@ -77,8 +77,8 @@ UPDATE pub.nimekirjad SET emem = 1 WHERE persoon IN
   FROM repis.kirjed k
   WHERE k.allikas IN (
     SELECT kood FROM repis.allikad 
-	  WHERE ifnull(nonPerson,0) = 0
-	    AND kood NOT IN ('kirm','elk','PR','RR')
+    WHERE ifnull(nonPerson,0) = 0
+      AND kood NOT IN ('kirm','elk','PR','RR')
   )
   and k.EkslikKanne = ''
   and k.Peatatud = ''
