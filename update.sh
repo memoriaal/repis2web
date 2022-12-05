3f20f94249d9e16f116c00c408f74c8d533e4283
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2022-12-05 14:44:56 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,8 +2,10 @@
 
 echo $(date -u --iso-8601=seconds) Started update
 
-cd "$(dirname "$0")"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd "$SCRIPT_DIR"
 
 git pull
 
-# . ./repis_update_2.sh
+./repis_update_2.sh
