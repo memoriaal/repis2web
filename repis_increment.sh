b7c7eea1958355c0583b8b4e7077196662575bc2
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2022-12-21 22:11:02 +0200

    Update repis_increment.sh

--- repis_increment.sh
+++ repis_increment.sh
@@ -67,7 +67,7 @@ sed -i 's/\\\"/\"\"/g' $csv_filename
 sed -i 's/\\\\\"\"/\\\"\"/g' $csv_filename
 
 echo $(date -u --iso-8601=seconds) uploading $csv_filename to memoriaal.ee
-MODE=update SOURCE=${csv_filename} ES_INDEX=emem_persons ES_CREDENTIALS="${ES_CREDENTIALS}" ES_HOST="${ES_HOST}" node pub3elastic.js > /home/michelek/Documents/scripts/repis2web/o.out
+MODE=update SOURCE=${csv_filename} ES_INDEX=emem_persons ES_CREDENTIALS="${ES_CREDENTIALS}" ES_HOST="${ES_HOST}" node pub3elastic.js >> /home/michelek/Documents/scripts/repis2web/o.out
 
 ####
 #### Wrap up
