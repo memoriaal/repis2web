d83f2e9d41885d58e8fc2d6c290b710de19a5498
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2022-12-21 18:51:58 +0200

    incremental update

--- repis_update_2.sh
+++ repis_update_2.sh
@@ -19,8 +19,19 @@ EOFMYSQL
 ####
 #### pub.nimekirjad
 ####
-echo $(date -u --iso-8601=seconds) Recreate table pub.nimekirjad
-mysql --port=3306 -u"${M_MYSQL_U}" -p"${M_MYSQL_P}" pub < recreate_pub_nimekirjad.sql
+last_ts=`cat last_ts.out`
+
+new_ts=`mysql --port=3306 -u"${M_MYSQL_U}" -p"${M_MYSQL_P}" pub<<EOFMYSQL
+select max(updated) as ts from pub.nimekirjad;
+EOFMYSQL
+`
+new_ts=`echo $new_ts | cut -d " " -f2,3`
+echo $new_ts > last_ts.out
+
+echo "$last_ts <--> $new_ts"
+
+# echo $(date -u --iso-8601=seconds) Recreate table pub.nimekirjad
+# mysql --port=3306 -u"${M_MYSQL_U}" -p"${M_MYSQL_P}" pub < recreate_pub_nimekirjad.sql
 
 
 ####
@@ -39,7 +50,7 @@ SELECT persoon, kirje, evokirje, perenimi, eesnimi, isanimi, emanimi
 , case when evo IS TRUE then 1 ELSE 0 end evo
 , case when wwiiref IS TRUE then 1 ELSE 0 end wwiiref
 from pub.nimekirjad
--- WHERE emem
+WHERE updated BETWEEN DATE_ADD('${last_ts}', INTERVAL 1 second) AND '${new_ts}'
 INTO OUTFILE '${csv_filename}'
 CHARACTER SET utf8
 FIELDS TERMINATED BY ','
@@ -54,52 +65,6 @@ sed -i 's/\\\\\"\"/\\\"\"/g' $csv_filename
 
 echo $(date -u --iso-8601=seconds) uploading $csv_filename to memoriaal.ee
 SOURCE=${csv_filename} ES_INDEX=emem_persons ES_CREDENTIALS="${ES_CREDENTIALS}" ES_HOST="${ES_HOST}" node pub3elastic.js > o.out
-
-# ####
-# #### memoriaal.ee EVO
-# ####
-# csv_filename="/paringud/$(date +%Y%m%d_%H%M%S)_memoriaal_ee_evo.csv"
-# echo $(date -u --iso-8601=seconds) Exporting to $csv_filename
-# mysql -u"${M_MYSQL_U}" -p"${M_MYSQL_P}" pub<<EOFMYSQL
-# SELECT persoon, kirje, evokirje, perenimi, eesnimi, isanimi, emanimi, sünd, surm, kirjed, pereseosed, tahvlikirje, emem, evo, wwiiref from pub.nimekirjad
-# WHERE evo
-# INTO OUTFILE '${csv_filename}'
-# CHARACTER SET utf8
-# FIELDS TERMINATED BY ','
-# ENCLOSED BY '"'
-# ESCAPED BY '\\\'
-# LINES TERMINATED BY '\n';
-# EOFMYSQL
-
-# sed -i 's/\\\"/\"\"/g' $csv_filename
-# sed -i 's/\\\\\"\"/\\\"\"/g' $csv_filename
-
-# echo $(date -u --iso-8601=seconds) uploading $csv_filename to memoriaal.ee
-# INDEX=evo_persons SOURCE=${csv_filename} ES_CREDENTIALS="${ELASTIC_C}" node pub2elastic.js
-
-
-# ####
-# #### wwii-refugees.ee
-# ####
-# csv_filename="/paringud/$(date +%Y%m%d_%H%M%S)_wwiiref.csv"
-# echo $(date -u --iso-8601=seconds) Exporting to $csv_filename
-# mysql -u"${M_MYSQL_U}" -p"${M_MYSQL_P}" pub<<EOFMYSQL
-# SELECT persoon, kirje, evokirje, perenimi, eesnimi, isanimi, emanimi, sünd, surm, kirjed, pereseosed, tahvlikirje, emem, evo, wwiiref from pub.nimekirjad
-# WHERE wwiiref
-# INTO OUTFILE '${csv_filename}'
-# CHARACTER SET utf8
-# FIELDS TERMINATED BY ','
-# ENCLOSED BY '"'
-# ESCAPED BY '\\\'
-# LINES TERMINATED BY '\n';
-# EOFMYSQL
-
-# sed -i 's/\\\"/\"\"/g' $csv_filename
-# sed -i 's/\\\\\"\"/\\\"\"/g' $csv_filename
-
-# echo $(date -u --iso-8601=seconds) uploading $csv_filename to wwii-refugees.ee
-# INDEX=wwii_persons SOURCE=${csv_filename} ES_CREDENTIALS="${ELASTIC_C}" node pub2elastic.js
-
 
 ####
 #### Wrap up
