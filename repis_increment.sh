eff7c5a4bdffc50ebe5485380fd4b5af43164f3c
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2022-12-22 11:06:52 +0200

    Update repis_increment.sh

--- repis_increment.sh
+++ repis_increment.sh
@@ -70,7 +70,7 @@ scp dev.memoriaal.ee:${csv_filename} ${csv_filename}
 sed -i 's/\\\"/\"\"/g' $csv_filename
 sed -i 's/\\\\\"\"/\\\"\"/g' $csv_filename
 
-echo $(date -u --iso-8601=seconds) uploading $csv_filename to memoriaal.ee
+# echo $(date -u --iso-8601=seconds) uploading $csv_filename to memoriaal.ee
 MODE=update SOURCE=${csv_filename} ES_INDEX=emem_persons ES_CREDENTIALS="${ES_CREDENTIALS}" ES_HOST="${ES_HOST}" node pub3elastic.js | grep Uploaded
 
 ####
