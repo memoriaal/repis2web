2225b2d03df80f127a5cd9cdc312432bf271eca4
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2022-12-22 20:25:31 +0200

    Update pub3elastic.js

--- pub3elastic.js
+++ pub3elastic.js
@@ -144,7 +144,7 @@ async function bulk_upload(bulk) {
     console.log(Object.keys(e.meta), e.meta.body, '===X===')
   })
 
-  console.log(JSON.stringify({bulk, operations, bulkResponse}, null, 2))
+  // console.log(JSON.stringify({bulk, operations, bulkResponse}, null, 2))
 
   let bix = 0
   if (bulkResponse && bulkResponse.items) {
