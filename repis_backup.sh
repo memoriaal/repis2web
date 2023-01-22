9c856a38a1b5c9c62ee092c039b6ab8150134e74
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2023-01-22 18:29:11 +0200

    nightly repub

--- repis_backup.sh
+++ repis_backup.sh
@@ -7,6 +7,7 @@ mysql --port=3306 -u"${M_MYSQL_U}" -p"${M_MYSQL_P}" repis<<EOFMYSQL
 CALL backups.backup_table('repis.kirjed');
 CALL backups.backup_table('repis.v_kirjelipikud');
 CALL backups.backup_table('repis.v_kirjesildid');
+CALL pub.repub();
 EOFMYSQL
 
 echo $(date -u --iso-8601=seconds) Repis backup finished
