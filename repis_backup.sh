c4dd6a064433461f3b11df0f2fb18f7c8ef487fb
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2023-01-24 14:06:05 +0200

    statistika

--- repis_backup.sh
+++ repis_backup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 . ~/.env
+
+echo $(date -u --iso-8601=seconds) Refresh statistics
+mysql --port=3306 -u"${M_MYSQL_U}" -p"${M_MYSQL_P}" aruanded < statistika.sql
+
 echo $(date -u --iso-8601=seconds) Started repis backup
 
 mysql --port=3306 -u"${M_MYSQL_U}" -p"${M_MYSQL_P}" repis<<EOFMYSQL
--- statistika.sql
+++ statistika.sql
@@ -7,36 +7,32 @@ WHERE
     persoon = kirjekood;
 
 -- Kokku isikuid kodulehel
-SELECT
-    count(1) INTO @isikuid_veebis
-FROM
-    `aruanded`.`memoriaal_ee`;
+SELECT count(1) INTO @isikuid_veebis
+FROM pub.nimekirjad;
 
 -- Relevantseid isikuid kodulehel
-SELECT
-    count(1) INTO @relevantseid
-FROM
-    `aruanded`.`memoriaal_ee`
-WHERE
-    relevantne = 1;
+SELECT count(1) INTO @emem
+FROM pub.nimekirjad WHERE emem;
 
 -- Isikuid mälestusseinal
-SELECT
-    count(1) INTO @kivis
-FROM
-    `aruanded`.`memoriaal_ee`
-WHERE
-    kivi = 1;
+SELECT count(1) INTO @kivis
+FROM pub.nimekirjad WHERE kivi;
+
+-- põgenikke
+SELECT count(1) INTO @refugees
+FROM pub.nimekirjad WHERE wwiiref;
 
 INSERT INTO
     aruanded.statistika (
         isikuid_baasis,
         isikuid_veebis,
-        relevantseid,
-        kivis
+        emem,
+        kivis,
+        põgenikke
     )
 SELECT
     @isikuid_baasis,
     @isikuid_veebis,
     @relevantseid,
-    @kivis;
+    @kivis,
+    @refugees;
