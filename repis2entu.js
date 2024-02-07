44fca0d0188e98e334ed299f49709e3b73d1cde6
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2024-02-07 23:58:57 +0200

    Update repis2entu.js

--- repis2entu.js
+++ repis2entu.js
@@ -64,7 +64,7 @@ async function get_folderE() {
     method: 'GET',
     headers: {
       'Accept-Encoding': 'deflate',
-      'Authorization': `Bearer ${token}`
+      'Authorization': `Bearer ${entu.token}`
     }
   }
   const response = await fetch(url, options)
@@ -93,7 +93,7 @@ async function get_victimE() {
     method: 'GET',
     headers: {
       'Accept-Encoding': 'deflate',
-      'Authorization': `Bearer ${token}`
+      'Authorization': `Bearer ${entu.token}`
     }
   }
   const response = await fetch(url, options)
