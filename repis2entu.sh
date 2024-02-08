d3c465129ff9ba1c048d0252cec92382fcd51802
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2024-02-08 19:00:19 +0200

    bulk 500

--- repis2entu.sh
+++ repis2entu.sh
@@ -19,7 +19,7 @@ trap "rm -f -- '$mypidfile'" EXIT
 # ssh tunnel to mysql proxy (control file in ~/.ssh/config)
 ssh -f -N -T -M -L 3306:127.0.0.1:3306 repis-proxy
 
-bulk_size=50000
+bulk_size=500
 ####
 #### pub.nimekirjad
 ####
