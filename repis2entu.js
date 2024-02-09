f6e14bc98affb7b5ef53d5e5e3876b2f609f041e
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2024-02-09 17:35:51 +0200

    Update repis2entu.js

--- repis2entu.js
+++ repis2entu.js
@@ -11,8 +11,6 @@ const ENTU_HOST      = process.env.ENTU_HOST      || 'api.entu.app'
 const ENTU_AUTH_PATH = process.env.ENTU_AUTH_PATH || '/auth?account=emi'
 const ENTU_WRITE_KEY = process.env.ENTU_WRITE_KEY
 
-const stream = fs.createReadStream(SOURCE)
-
 // set working dir to script dir
 process.chdir(__dirname)
 
