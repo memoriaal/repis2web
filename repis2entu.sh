f5e5555932668288570df03378020f5828b5b158
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2024-02-09 17:45:51 +0200

    Update repis2entu.sh

--- repis2entu.sh
+++ repis2entu.sh
@@ -18,10 +18,12 @@ trap "rm -f -- '$mypidfile'" EXIT
 
 # ssh tunnel to mysql proxy (control file in ~/.ssh/config)
 ssh -f -N -T -M -L 3306:127.0.0.1:3306 repis-proxy
-`mysql --port=3306 -u"${M_MYSQL_U}" -p"${M_MYSQL_P}" pub<<EOFMYSQL
+
+new_ts=`mysql --port=3306 -u"${M_MYSQL_U}" -p"${M_MYSQL_P}" pub<<EOFMYSQL
 SELECT current_timestamp() as ts;
 EOFMYSQL
 `
+echo "new timestamp: ${new_ts}"
 node repis2entu.js
 
 
