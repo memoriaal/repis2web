fb1e7f80a3087376b8267226da3d9ed1f181f4a9
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2024-02-10 08:56:46 +0200

    Update repis2entu.js

--- repis2entu.js
+++ repis2entu.js
@@ -188,8 +188,8 @@ const run = async () => {
     }
     await pool.execute(update_q, [row.persoon, `${entu_id}`, `${entu_id}`])
     const [updated] = await pool.execute(select_updated, [row.persoon])
-    // console.log(counter, row.eesnimi, row.perenimi, row.updated)
-    console.log(counter, row.eesnimi, row.perenimi, row.updated, updated.persoon, updated.entu_id, updated.sync_ts)
+    console.log(counter, row.eesnimi, row.perenimi, row.updated, updated[0].persoon, updated[0].entu_id, updated[0].sync_ts)
+    // console.log(counter, row.eesnimi, row.perenimi, row.updated, updated.persoon, updated.entu_id, updated.sync_ts)
   }
   // connection.end()
   return persons
