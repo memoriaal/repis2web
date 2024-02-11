6c449aa3d6a11df4f2bb5a0a148daed106f62c72
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2024-02-11 21:08:05 +0200

    ES8 stuff

---
+++ es8/repis2es.js
@@ -0,0 +1,245 @@
+'use strict'
+
+const path         = require('path')
+const fs         = require('fs')
+const csv        = require('@fast-csv/parse')
+const fetch      = require('node-fetch')
+const mysql     = require('mysql2/promise')
+
+
+const ENTU_HOST      = process.env.ENTU_HOST      || 'api.entu.app'
+const ENTU_AUTH_PATH = process.env.ENTU_AUTH_PATH || '/auth?account=emi'
+const ENTU_WRITE_KEY = process.env.ENTU_WRITE_KEY
+
+// set working dir to script dir
+process.chdir(__dirname)
+
+const bulk_size = 200000
+const mysqlConfig = {
+  host: '127.0.0.1',
+  user: process.env.M_MYSQL_U,
+  password: process.env.M_MYSQL_P,
+  database: process.env.M_DB_NAME || 'pub',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+}
+const pool = mysql.createPool(mysqlConfig)
+
+const select_q = `
+  select e.entu_id, e.sync_ts, nk.*
+  from pub.nimekirjad nk
+  left join pub.entu e on e.persoon = nk.persoon
+  where e.sync_ts is null
+  order by nk.updated
+  limit ?;
+`
+const update_q = `
+  insert into pub.entu (persoon, entu_id, sync_ts) values (?, ?, current_timestamp())
+  on duplicate key update entu_id = ?, sync_ts = current_timestamp();
+`
+const select_updated = `
+  select e.*
+  from pub.entu e
+  where e.persoon = ?;
+`
+
+
+const get_token = async () => {
+  const url = `https://${ENTU_HOST}${ENTU_AUTH_PATH}`
+  const options = {
+    method: 'GET',
+    headers: {
+      'Accept-Encoding': 'deflate',
+      'Authorization': `Bearer ${ENTU_WRITE_KEY}`
+    }
+  }
+  const response = await fetch(url, options)
+  const json = await response.json()
+  if (Array.isArray(json) && json.length > 0) {
+    if (json[0].token) {
+      return json[0].token
+    } else {
+      console.error('no token in json data')
+      return null
+    }
+  } else {
+    console.error('get_token: Invalid json data')
+    return null
+  }
+}
+
+const get_folderE = async () => {
+  const url = `https://${ENTU_HOST}/entity?_type.string=folder&name.string=Publitseeritud+kirjed&props=_id`
+  const options = {
+    method: 'GET',
+    headers: {
+      'Accept-Encoding': 'deflate',
+      'Authorization': `Bearer ${entu.token}`
+    }
+  }
+  const response = await fetch(url, options)
+  const json = await response.json()
+  if (json.entities && Array.isArray(json.entities) && json.entities.length > 0) {
+    if (json.entities[0]._id) {
+      return json.entities[0]._id
+    } else {
+      console.error('no _id in json data')
+      return null
+    }
+  } else {
+    console.error('get_folderE: Invalid json data', {json, entities: json.entities, length: json.entities.length})
+    return null
+  }
+}
+
+const get_victimE = async () => {
+  const url = `https://${ENTU_HOST}/entity?_type.string=entity&name.string=victim&props=_id`
+  const options = {
+    method: 'GET',
+    headers: {
+      'Accept-Encoding': 'deflate',
+      'Authorization': `Bearer ${entu.token}`
+    }
+  }
+  const response = await fetch(url, options)
+  const json = await response.json()
+  if (json.entities && Array.isArray(json.entities) && json.entities.length > 0) {
+    if (json.entities[0]._id) {
+      return json.entities[0]._id
+    } else {
+      console.error('no _id in json data')
+      return null
+    }
+  } else {
+    console.error('get_victimE: Invalid json data', {json, entities: json.entities, length: json.entities.length})
+    return null
+  }
+}
+
+const row2entity = (row) => {
+  const entity = []
+
+  entity.push({ "type": "_type", "reference": entu.victimE })
+  
+  if (!row.persoon) { return false }
+  entity.push({ "type": "persoon", "string": row.persoon })
+  
+  if (row.redirect) {
+    entity.push({ "type": "redirect", "string": row.redirect })
+    return entity
+  }
+  
+  if (!row.kirje) {
+    console.log(`Missing kirje for persoon ${row.persoon}`)
+    return false 
+  }
+  if (!/[a-zõüöäA-ZÕÜÖÄ]/.test(row.kirje)) {
+    console.log(`Invalid kirje for persoon ${row.persoon}: ${row.kirje}`)
+    return false
+  }
+
+  entity.push({ "type": "kirje", "string": row.kirje })
+
+  if (!row.eesnimi && !row.perenimi) { 
+    console.log(`Missing forename and surname for persoon ${row.persoon}`)
+    return false 
+  }
+  row.eesnimi && entity.push({ "type": "forename", "string": row.eesnimi })
+  row.perenimi && entity.push({ "type": "surname", "string": row.perenimi })
+
+  row.evokirje && entity.push({ "type": "evokirje", "string": row.evokirje })
+  row.father && entity.push({ "type": "father", "string": row.isanimi })
+  row.mother && entity.push({ "type": "mother", "string": row.emanimi })
+  row.birth && entity.push({ "type": "birth", "string": row.sünd })
+  row.death && entity.push({ "type": "death", "string": row.surm })
+  row.birthplace && entity.push({ "type": "birthplace", "string": row.sünnikoht })
+  row.deathplace && entity.push({ "type": "deathplace", "string": row.surmakoht })
+  
+  row.kirjed && entity.push({ "type": "kirjed", "string": row.kirjed })
+  row.pereseosed && (row.pereseosed !== '[]') && entity.push({ "type": "pereseosed", "string": row.pereseosed })
+  row.tahvlikirje && (row.tahvlikirje !== '{}') && entity.push({ "type": "tahvlikirje", "string": row.tahvlikirje })
+  row.episoodid && (row.episoodid !== '[]') && entity.push({ "type": "episoodid", "string": row.episoodid })
+  
+  row.isperson === '1' && entity.push({ "type": "isperson", "boolean": true })
+  row.kivi === '1' && entity.push({ "type": "kivi", "boolean": true })
+  row.emem === '1' && entity.push({ "type": "emem", "boolean": true })
+  row.evo === '1' && entity.push({ "type": "evo", "boolean": true })
+  row.wwii === '1' && entity.push({ "type": "wwii", "boolean": true })
+  row.mv === '1' && entity.push({ "type": "mv", "boolean": true })
+  
+  entity.push({ "type": "_parent", "reference": entu.folderE })
+
+  return entity
+}
+
+const entu = {}
+const run = async () => {
+  entu.token = await get_token()
+  entu.folderE = await get_folderE()
+  entu.victimE = await get_victimE()
+  
+  // const connection = await mysql.createConnection(mysqlConfig)
+  const [rows, fields] = await pool.execute(select_q, [bulk_size])
+  console.log({fields: fields.map(f => f.name)})
+  const persons = rows.map(r => r.persoon)
+  let counter = 0
+  for (let row of rows) {
+    counter++
+    let entu_id = false
+    // while (!entu_id) {
+      entu_id = await entu_post(row)
+      if (!entu_id) {
+        console.log(counter, row.persoon, 'not posted to entu')
+        // wait a second and try again
+        await wait_a_sec()
+        continue
+      }
+    // }
+    await pool.execute(update_q, [row.persoon, `${entu_id}`, `${entu_id}`])
+    const [updated] = await pool.execute(select_updated, [row.persoon])
+    console.log(counter, row.persoon, row.updated, updated[0].entu_id, updated[0].sync_ts, row.eesnimi, row.perenimi)
+    // console.log(counter, row.eesnimi, row.perenimi, row.updated, updated.persoon, updated.entu_id, updated.sync_ts)
+  }
+  // connection.end()
+  return persons
+}
+
+run()
+.catch(console.log)
+.then((msg) => {
+  console.log('done', msg)
+  // process.exit(0)
+})
+
+const entu_post = async (row) => {
+  // const entu_id = `entu_${row.persoon}`
+  const entity = row2entity(row)
+  if (!entity) {
+    // console.error('entu_post: Invalid entity', {row})
+    return false
+  }
+  const url = `https://${ENTU_HOST}/entity`
+  const entu_options = {
+    method: 'POST',
+    headers: {
+      'Accept-Encoding': 'deflate',
+      'Authorization': `Bearer ${entu.token}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(entity)
+  }
+  const response = await fetch(url, entu_options)
+  response.ok || console.error(`entu_post response error: ${response.status} ${response.statusText} | Persoon: ${row.persoon}`)
+  const json = await response.json()
+  if (json._id) {
+    return json._id
+  } else {
+    console.error(`entu_post Invalid json data: ${response.status} ${response.statusText} | Persoon: ${row.persoon}`)
+    return false
+  }
+}
+
+const wait_a_sec = async () => {
+  return new Promise(resolve => setTimeout(resolve, 1000))
+}
---
+++ es8/repis2es.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+cd /home/michelek/Documents/scripts/repis2web
+mypidfile=/home/michelek/Documents/scripts/repis2web/es8/repis2entu.sh.pid
+
+if [ -e $mypidfile ]; then
+    echo -n ". "
+    exit 0
+fi
+
+echo $$ > "$mypidfile"
+# Ensure PID file is removed on program exit.
+trap "rm -f -- '$mypidfile'" EXIT
+
+# echo workin\' at `pwd`
+
+. /home/michelek/.env
+
+# ssh tunnel to mysql proxy (control file in ~/.ssh/config)
+ssh -f -N -T -M -L 3306:127.0.0.1:3306 repis-proxy
+
+
+####
+#### pub.nimekirjad
+####
+last_ts=`cat last_ts.out`
+# echo "last timestamp: ${last_ts}"
+
+
+bulksize=42
+
+new_ts=`mysql --port=3306 -u"${M_MYSQL_U}" -p"${M_MYSQL_P}" pub<<EOFMYSQL
+CALL pub.repub(${bulksize});
+select max(updated) as ts from pub.nimekirjad;
+EOFMYSQL
+`
+new_ts=`echo $new_ts | cut -d " " -f2,3`
+# echo "new timestamp: ${new_ts}"
+
+if [ "${new_ts}" == "" ]
+then
+    echo Failed to fetch timestamp from database.
+    exit 1
+fi
+
+echo $new_ts > last_ts.out
+
+if [ "${new_ts}" == "${last_ts}" ]
+then
+    echo $(date -u --iso-8601=seconds) no news after $new_ts
+    exit 0
+fi
+
+# echo "Updating between $last_ts <--> $new_ts"
+
+
+####
+#### memoriaal.ee emem
+####
+csv_filename="/paringud/$(date +%Y%m%d_%H%M%S)_memoriaal_ee_emem.csv"
+# echo $(date -u --iso-8601=seconds) Exporting to $csv_filename
+mysql --port=3306 -u"${M_MYSQL_U}" -p"${M_MYSQL_P}" pub<<EOFMYSQL
+SELECT persoon, kirje, evokirje, perenimi, eesnimi, isanimi, emanimi
+, left(sünd,10), left(surm,10)
+, sünnikoht, surmakoht
+, kirjed, pereseosed, tahvlikirje, episoodid
+, case when isperson IS TRUE then 1 ELSE 0 end isperson
+, case when kivi IS TRUE then 1 ELSE 0 end kivi
+, case when emem IS TRUE then 1 ELSE 0 end emem
+, case when evo IS TRUE then 1 ELSE 0 end evo
+, case when wwiiref IS TRUE then 1 ELSE 0 end wwiiref
+, case when mv IS TRUE then 1 ELSE 0 end mv
+, redirect
+from pub.nimekirjad
+WHERE updated BETWEEN DATE_ADD('${last_ts}', INTERVAL 1 second) AND '${new_ts}'
+INTO OUTFILE '${csv_filename}'
+CHARACTER SET utf8
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+ESCAPED BY '\\\'
+LINES TERMINATED BY '\n';
+EOFMYSQL
+
+scp dev.memoriaal.ee:${csv_filename} ${csv_filename}
+sed -i 's/\\\"/\"\"/g' $csv_filename
+sed -i 's/\\\\\"\"/\\\"\"/g' $csv_filename
+
+# echo $(date -u --iso-8601=seconds) uploading $csv_filename to memoriaal.ee
+echo -n "$(date -u --iso-8601=seconds) Between ${last_ts} and ${new_ts}: "
+MODE=update SOURCE=${csv_filename} ES_INDEX=emi_persons E8_CREDENTIALS="${ES8_CREDENTIALS}" ES_HOST="${ES8_HOST}" node repis2es.js | grep Uploaded
+
+####
+#### Wrap up
+####
+
+rm ${csv_filename}
+ssh dev.memoriaal.ee "rm ${csv_filename}"
+
+# close the ssh tunnel
+ssh -T -O "exit" repis-proxy
+
+# echo $(date -u --iso-8601=seconds) Repis update finished
